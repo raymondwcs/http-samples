b848f24070f744541a4af8ef4d64184159f824fb
Raymond SO <rso@ouhk.edu.hk>
2015-10-08 08:48:35 +0800

    simple HTTP Node.js scripts

---
+++ greetings/sayHelloWithTime.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+var server = http.createServer(function (req,res) {
+	var today = new Date();
+
+	console.log("INCOMING REQUEST: " + req.method + " " + req.url);
+	res.writeHead(200, {"Content-Type" : "text/html"});
+	res.write('<html><head><title>sayHello</title></head>');
+	res.write('<body><H1>Hello There!</H1></body>');
+	res.write('<p>It is now ' + today.toTimeString() + '</p>');
+	res.end('</html>');
+});
+
+server.listen(process.env.PORT || 8099);
