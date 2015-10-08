f1f7ed05018a7ad661f764561efcaa525f749a2e
Raymond SO <rso@ouhk.edu.hk>
2015-10-08 16:14:06 +0800

    fixed bugs

--- serveJPGFile.js
+++ serveJPGFile.js
@@ -6,26 +6,33 @@ function handle_incoming_request(req, res) {
 	console.log("INCOMING REQUEST: " + req.method + " " + req.url);
 	var parsedURL = url.parse(req.url,true); //true to get query as object 
 	var queryAsObject = parsedURL.query;
-	var fname = queryAsObject.fname;
 
-	console.log('Requested file: ' + fname);
-
-	fs.exists(fname, function(exists) {
-		if (exists) {
-			console.log('Opening file: ' + fname);
-			fs.readFile(fname, function(err,data) {
-				res.writeHead(200, {'Content-Type': 'image/jpeg'});
-				res.write(data);
+	if (null != queryAsObject.fname) {
+		console.log('Requested file: ' + fname);
+		var fname = queryAsObject.fname;
+		fs.exists(fname, function(exists) {
+			if (exists) {
+				console.log('Opening file: ' + fname);
+				fs.readFile(fname, function(err,data) {
+					res.writeHead(200, {'Content-Type': 'image/jpeg'});
+					res.write(data);
+					res.end();
+				}); // end fs.readFile()
+			} // end if (exists)
+			else {
+				console.log('File not found: ' + fname);
+				res.writeHead(404, {'Content-Type': 'text/plain'});
+				res.write('404 Not Found\n');
 				res.end();
-			}); // end fs.readFile()
-		} // end if (exists)
-		else {
-			console.log('File not found: ' + fname);
-			res.writeHead(404, {'Content-Type': 'text/plain'});
-			res.write('404 Not Found\n');
-			res.end();
-		}
-	}) // end fs.exists()
+			}
+		}) // end fs.exists()
+	} // end if
+	else {
+		console.log('File name missing!');
+		res.writeHead(422, {'Content-Type': 'text/plain'});
+		res.write('404 Not Found\n');
+		res.end();
+	}
 
 }
 
