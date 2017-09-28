8b7e177fb90ae8c9444d98d218963adc3888ece4
Raymond So <rso@ouhk.edu.hk>
2017-09-28 15:05:27 +0800

    added: returns result in json format

--- rectangle/server.js
+++ rectangle/server.js
@@ -13,25 +13,29 @@ function handle_incoming_request(req, res) {
    var parsedURL = url.parse(req.url,true);  //true to get query as object
    switch(parsedURL.pathname) {
       case '/area':
+      case '/api/area':
          var queryAsObject = parsedURL.query;
          var obj = new Rectangle(queryAsObject.width, queryAsObject.length); 
-         res.writeHead(200, {"Content-Type" : "text/html"});
-         res.write("<html>");
-         res.write("<title>Rectangle Area Calculator</title>");
-         res.write('<meta name="viewport" content="width=device-width, initial-scale=1">');
-         res.write('<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">');
-         res.write('<body>');
-         res.write('<div class="w3-container">');         
-         res.write("<p>Area of rectangles having length <b>" + queryAsObject.length + "</b>, width <b>" +
-                   queryAsObject.width + "</b> is <b>" + obj.area + "</b></p>");
-         res.write("<br><a href='javascript:history.back()'>Back</a>");
-         res.write('</div>');         
-         res.write('</body>')         
-         res.write("</html>");
-         res.end();
+         if (parsedURL.pathname == '/area') {
+            res.writeHead(200, {"Content-Type" : "text/html"});
+            res.write("<html>");
+            res.write("<title>Rectangle Area Calculator</title>");
+            res.write('<meta name="viewport" content="width=device-width, initial-scale=1">');
+            res.write('<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">');
+            res.write('<body>');
+            res.write('<div class="w3-container">');         
+            res.write("<p>Area of rectangles having length <b>" + queryAsObject.length + "</b>, width <b>" +
+                      queryAsObject.width + "</b> is <b>" + obj.area + "</b></p>");
+            res.write("<br><a href='javascript:history.back()'>Back</a>");
+            res.write('</div>');         
+            res.write('</body>')         
+            res.write("</html>");
+            res.end();
+         } else {
+            res.writeHead(200, {"Content-Type" : "application/json"}); 
+            res.end(JSON.stringify(obj));;
+         }
          break;
-         // res.writeHead(200, {"Content-Type" : "application/json"}); 
-         // res.end(JSON.stringify(obj));;
       default:
          res.writeHead(200, {"Content-Type" : "text/html"});
          res.write("<html>");
