da943291e0eb6e3cb39df083a5e317cb5ac8de9b
Raymond So <rso@ouhk.edu.hk>
2017-10-11 10:55:26 +0800

    fixed minor bugs

--- mongodb/server.js
+++ mongodb/server.js
@@ -4,33 +4,28 @@ var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
 //var mongourl = 'mongodb://localhost:27017/test';
-var mongourl = 'mongodb://student:password@ds031873.mlab.com:31873/comps381f';
+var mongourl = 'mongodb://student:password@ds155587.mlab.com:55587/scttest06';
 
 var server = http.createServer(function (req,res) {
 	console.log("INCOMING REQUEST: " + req.method + " " + req.url);
 
 	var parsedURL = url.parse(req.url,true); //true to get query as object
 
-	if (!(
-		parsedURL.pathname == '/create' ||
-		parsedURL.pathname == '/read' ||
-		parsedURL.pathname == '/update' ||
-		parsedURL.pathname == '/delete' 
-		)) {
-		res.writeHead(404, {"Content-Type": "text/plain"});
-		res.write("404 Not Found\n");
-		res.end();
-	}
-	else {
-		switch(parsedURL.pathname) {
-			case "/read":
-				read_n_print(res);
-				break;
-			default:
-				res.writeHead(500, {"Content-Type": "text/plain"});
-				res.write(parsedURL.pathname + " not available yet\n");
-				res.end();
-		}
+	switch(parsedURL.pathname) {
+		case '/read':
+			read_n_print(res);
+			break;
+		case '/create':
+		case '/update':
+		case '/delete':
+			res.writeHead(500, {"Content-Type": "text/plain"});
+			res.write(parsedURL.pathname + " not available yet\n");
+			res.end();
+			break;
+		default:
+			res.writeHead(404, {"Content-Type": "text/plain"});
+			res.write("404 Not Found\n");
+			res.end();
 	}
 });
 
