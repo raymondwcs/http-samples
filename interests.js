f2653ff8b4d77ed54365a3cb98a8439b41a976a1
Raymond SO <rso@ouhk.edu.hk>
2019-08-21 14:48:50 +0800

    Simple interests

---
+++ interests.js
@@ -0,0 +1,23 @@
+const http = require('http'); 
+const url = require('url');
+
+function SimpleInterests(p,r,t) {
+	this.principal = p;
+	this.rate = r;
+	this.period = t;
+	this.interests = this.principal * this.rate * this.period;
+}
+
+function handle_incoming_request(req, res) {
+	console.log("INCOMING REQUEST: " + req.method + " " + req.url);
+	var parsedURL = url.parse(req.url,true); //true to get query as object 
+	var queryAsObject = parsedURL.query;
+	// extract query string parameters
+	var obj = new SimpleInterests(queryAsObject.p, queryAsObject.r,queryAsObject.t); 
+
+	res.writeHead(200, {"Content-Type" : "application/json"}); 
+	res.end(JSON.stringify(obj));;
+}
+
+var s = http.createServer(handle_incoming_request); 
+s.listen(process.env.PORT || 8099);
