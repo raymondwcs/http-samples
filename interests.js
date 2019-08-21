e50d95bde68a482b2e0a7c48c3b7e36093ac6ba4
Raymond SO <rso@ouhk.edu.hk>
2019-08-21 20:19:01 +0800

    use more ES6 syntax

--- interests.js
+++ interests.js
@@ -1,20 +1,21 @@
 const http = require('http'); 
 const url = require('url');
 
-function SimpleInterests(p,r,t) {
-	this.principal = p;
-	this.rate = r;
-	this.period = t;
-	this.interests = this.principal * this.rate * this.period;
+class SimpleInterests {
+	constructor(p,r,t) {
+		this.principal = p;
+		this.rate = r;
+		this.period = t;
+		this.interests = this.principal * this.rate * this.period;
+	}
 }
 
-function handle_incoming_request(req, res) {
+handle_incoming_request = (req, res) => {
 	console.log("INCOMING REQUEST: " + req.method + " " + req.url);
 	var parsedURL = url.parse(req.url,true); //true to get query as object 
 	var queryAsObject = parsedURL.query;
 	// extract query string parameters
 	var obj = new SimpleInterests(queryAsObject.p, queryAsObject.r,queryAsObject.t); 
-
 	res.writeHead(200, {"Content-Type" : "application/json"}); 
 	res.end(JSON.stringify(obj));;
 }
