ad63aaab6d0bb8e82ed389c7b7c42e8a893761b9
Raymond SO <rso@ouhk.edu.hk>
2019-09-23 19:55:19 +0800

    used more ES6 syntax

--- rectangle/server.js
+++ rectangle/server.js
@@ -1,16 +1,19 @@
-var http = require('http'); 
-var url = require('url');
+const http = require('http'); 
+const url = require('url');
 
-function Rectangle(width,length) { 
-	this.width = width;
-	this.length = length;
-	this.area = this.width * this.length;
+class Rectangle {
+   constructor (width,length) { 
+      this.width = width;
+      this.length = length;
+      this.area = this.width * this.length;
+   }
 }
 
-function handle_incoming_request(req, res) {
+const server = http.createServer((req, res) => {
    console.log("INCOMING REQUEST: " + req.method + " " + req.url);
    
    var parsedURL = url.parse(req.url,true);  //true to get query as object
+
    switch(parsedURL.pathname) {
       case '/area':
       case '/api/area':
@@ -24,13 +27,11 @@ function handle_incoming_request(req, res) {
             res.write('<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">');
             res.write('<body>');
             res.write('<div class="w3-container">');         
-            res.write("<p>Area of rectangles having length <b>" + queryAsObject.length + "</b>, width <b>" +
-                      queryAsObject.width + "</b> is <b>" + obj.area + "</b></p>");
+            res.write(`<p>Area of rectangles having length <b> ${queryAsObject.length} </b>, width <b> ${queryAsObject.width}</b> is <b> ${obj.area} </b></p>`);
             res.write("<br><a href='javascript:history.back()'>Back</a>");
             res.write('</div>');         
             res.write('</body>')         
-            res.write("</html>");
-            res.end();
+            res.end("</html>");
          } else {
             res.writeHead(200, {"Content-Type" : "application/json"}); 
             res.end(JSON.stringify(obj));;
@@ -53,10 +54,8 @@ function handle_incoming_request(req, res) {
          res.write("</form>");
          res.write('</div>');
          res.write('</body>')
-         res.write("</html>");    
-         res.end();     
+         res.end("</html>");    
    }
-}
+});
 
-var s = http.createServer(handle_incoming_request); 
-s.listen(process.env.PORT || 8099);
+server.listen(process.env.PORT || 8099);
