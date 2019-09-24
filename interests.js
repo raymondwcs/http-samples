94ae5701e9f377abba0f949898a38dac63f96f19
Raymond So <rso@ouhk.edu.hk>
2019-09-24 08:26:47 +0800

    updated

--- interests.js
+++ interests.js
@@ -8,24 +8,43 @@ function SimpleInterests() {
 	this.interests = this.principal * this.rate * this.period;
 }
 */
+const principal = 1000000;
+const rate = 0.01;
+const period = 10;
+
 class SimpleInterests {
-	constructor(p,r,t) {
+	constructor(p = principal,r = rate,t = period) { // default parameter values
 		this.principal = p;
 		this.rate = r;
 		this.period = t;
 		this.interests = this.principal * this.rate * this.period;
 	}
+
+	toString() {
+		return(`Interests for $${this.principal} at an annual rate of \
+${this.rate *100}% for ${this.period} years = $${this.interests}`);
+	}
 }
 
 // function handle_incoming_request(req,res) {
 const handle_incoming_request = (req, res) => {
-	console.log("INCOMING REQUEST: " + req.method + " " + req.url);
-	var parsedURL = url.parse(req.url,true); //true to get query as object 
-	var queryAsObject = parsedURL.query;
+	let timestamp = new Date().toISOString();
+    console.log(`Incoming request ${req.method}, ${req.url} received at ${timestamp}`);
+
+	var parsedURL = url.parse(req.url,true); // true to get query as object 
+
 	// extract query string parameters
-	var obj = new SimpleInterests(queryAsObject.p, queryAsObject.r,queryAsObject.t); 
-	res.writeHead(200, {"Content-Type" : "application/json"}); 
-	res.end(JSON.stringify(obj));;
+	var obj = new SimpleInterests(parsedURL.query.p, parsedURL.query.r,parsedURL.query.t); 
+
+	if (parsedURL.query.format == 'json') {
+		res.writeHead(200, {"Content-Type" : "text/json"}); 
+		res.end(JSON.stringify(obj));
+	} else {
+		res.writeHead(200, {"Content-Type" : "text/html"});
+		res.write('<html><h1>');
+		res.write(obj.toString());
+		res.end('</h1></html>');
+	}
 }
 
 const server = http.createServer(handle_incoming_request); 
