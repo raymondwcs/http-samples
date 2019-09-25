ee82dff88a3a4e2a11ed26e63b116961f2894ae3
Raymond So <rso@ouhk.edu.hk>
2019-09-25 13:38:53 +0800

    used more ES6 syntax

--- greetings/sayHelloWithTime.js
+++ greetings/sayHelloWithTime.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const server = http.createServer(function (req,res) {
+const server = http.createServer((req,res) => {
 	let today = new Date();
 	
 	let timestamp = new Date().toISOString();
