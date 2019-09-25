ef8e20073311a97bd251532e349d13933da472b3
Raymond So <rso@ouhk.edu.hk>
2019-09-25 12:53:15 +0800

    used more ES6 syntax

--- http_post/server.js
+++ http_post/server.js
@@ -1,26 +1,31 @@
-var http = require('http');
-var qs = require ('querystring');
+const http = require('http');
+const qs = require ('querystring');
 
-formServer = http.createServer(function(req,res) {
-   console.log('Request: '+req.url);
+const formServer = http.createServer((req,res) => {
+   let timestamp = new Date().toISOString();
+   console.log(`Incoming request ${req.method}, ${req.url} received at ${timestamp}`);
+   
    if (req.method == 'POST') {
-      var data = '';  // message body data
+      let data = '';  // message body data
 
       // process data in message body
-      req.on('data', function(chunk) {
-         data += chunk;
+      req.on('data', (payload) => {
+         data += payload;
       });
 
       // when no more data in message body
-      req.on('end', function() {  
-         var post = qs.parse(data);
+      req.on('end', () => {  
+         let postdata = qs.parse(data);
          res.writeHead(200, {'Content-Type': 'text/html'}); 
          res.write('<html>')        
-         res.write('User Name = ' + post.name);
+         res.write(`User Name = ${postdata.name}`);
          res.write('<br>')
-         res.write('Password = ' + post.password);
+         res.write(`Password = ${postdata.password}`);
          res.end('</html>')                 
       })
+   } else {
+      res.writeHead(404, {'Content-Type': 'text/plain'}); 
+      res.end('I can only handle POST request!!! Sorry.')
    }
 });
 formServer.listen(process.env.PORT || 8099);
