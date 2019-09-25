e369f355033d2572d14beaf87c42af58b7452034
Raymond So <rso@ouhk.edu.hk>
2019-09-25 09:25:42 +0800

    used more ES6 syntax

--- echo/echo.js
+++ echo/echo.js
@@ -1,38 +1,34 @@
-var http = require('http'); 
-var url = require('url');
+const http = require('http'); 
+const url = require('url');
 
-function handle_incoming_request(req,res) {
-   console.log("Incoming request: " + req.url);
-   console.log("Request type: " + req.method);
-   var parsedURL = url.parse(req.url,true);  // request url -> json object
+const handle_incoming_request = (req,res) => {
+   let timestamp = new Date().toISOString();
+	console.log(`Incoming request ${req.method}, ${req.url} received at ${timestamp}`);
+
+	var parsedURL = url.parse(req.url,true); // true to get query as object 
 
    res.writeHead(200,{"Content-Type" : "text/html"});
    res.write("<html><body>");
-   res.write("Request method: " + req.method);
-   res.write('<br>');
-   res.write("Request path: " + parsedURL.pathname);
-   res.write('<br>');   
-   res.write("Query String: " + url.parse(req.url).query);
-   res.write('<br>');   
+   res.write(`Request method: ${req.method} <br>`);
+   res.write(`Request path: ${parsedURL.pathname}<br>`);
+   res.write(`Query String: ${url.parse(req.url).query}<br><br>`);
  
-   var queryAsObject = parsedURL.query;  // query string (json object)
-   console.log(JSON.stringify(queryAsObject));
-   var number_of_query_string_parameters = Object.keys(queryAsObject).length;
+   console.log(`Pathname: ${parsedURL.pathname}`)
+   console.log(`Query string parameter(s): ${JSON.stringify(parsedURL.query)}`);
+   
+   var number_of_query_string_parameters = Object.keys(parsedURL.query).length;
    if (number_of_query_string_parameters > 0) {
-      res.write("No. of query string parameter(s): " + 
-                number_of_query_string_parameters);
-      res.write('<br>');      
-      for (key in queryAsObject) {
-         res.write(key + " = " + queryAsObject[key]);
-         res.write('<br>');         
+      res.write(`No. of query string parameter(s): ${number_of_query_string_parameters}<br>`);
+      for (key in parsedURL.query) {
+         res.write(`key = ${parsedURL.query[key]}<br>`);
       }
    } else {
-      res.write('There is no query string parameter!')
+      res.write('There is no query string parameter!<br>')
    }
 
-   res.write("<br><br>User Agent: " + req.headers['user-agent']);
+   res.write(`<br>User Agent: ${req.headers['user-agent']}`);
    res.end("</body></html>");   
 }
 
-var server = http.createServer(handle_incoming_request);
+const server = http.createServer(handle_incoming_request);
 server.listen(process.env.PORT ||  8099);
