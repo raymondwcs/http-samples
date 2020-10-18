1425523a06476588f30e8c04a7fecff295ab769c
Raymond SO <rso@ouhk.edu.hk>
2020-10-18 13:48:03 +0800

    fixed minor bugs

--- greetings/sayHelloWithTime.js
+++ greetings/sayHelloWithTime.js
@@ -8,9 +8,9 @@ const server = http.createServer((req,res) => {
 
 	res.writeHead(200, {"Content-Type" : "text/html"});
 	res.write('<html><head><title>sayHello</title></head>');
-	res.write('<body><H1>Hello There!</H1></body>');
-	res.write(`<p>It is now ${today.toTimeString()} </p>`);
-	res.end('</html>');
+	res.write('<body><H1>Hello There!</H1>');
+	res.write(`<p>It is now ${today.toTimeString()}</p>`);
+	res.end('</body></html>');
 });
 
 server.listen(process.env.PORT || 8099);
