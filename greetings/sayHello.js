228cdce593d420af3679bba30a798cefdef4a8b6
Raymond SO <rso@ouhk.edu.hk>
2020-10-21 18:34:34 +0800

    updated

--- greetings/sayHello.js
+++ greetings/sayHello.js
@@ -6,10 +6,10 @@ const server = http.createServer((req,res) => {
 	let timestamp = new Date().toISOString();
 	console.log(`Incoming request ${req.method}, ${req.url} received at ${timestamp}`);	
 
-	res.writeHead(200, {"Content-Type" : "text/html"});
-	res.write('<html><head><title>sayHello</title></head>');
+	res.writeHead(200, {"Content-Type" : "text/html"});  // send response header
+	res.write('<html><head><title>sayHello</title></head>');  // send response body
 	res.write('<body><H1>Hello There!</H1></body>');
-	res.end('</html>');
+	res.end('</html>');  // send last piece of response body & end connection
 });
 
 // server listen on port 8099 for inbound http connection requests
