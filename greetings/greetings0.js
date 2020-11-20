d8568d9a75a0a644ac14e72734058e3d0e435598
Raymond SO <rso@ouhk.edu.hk>
2020-11-20 10:25:01 +0800

    updated

--- greetings/greetings0.js
+++ greetings/greetings0.js
@@ -1,9 +1,17 @@
 const http = require('http');
 const url  = require('url');
 
-const server = http.createServer(function (req,res) {
-	const greetingMsg = "Hello there!";
+const greetingMsg = (name = null, includeTime = false) => {
+	let today = new Date();
+	let msg = (name != null) ? 'Hello ' + name + '! ' : 'Hello there!';
+	if (includeTime) {
+	  msg += `  It is now ${today.toTimeString()}`;
+	  msg = `<html><head><title>sayHello</title></head><body><H1>${msg}</H1></body><html>`;
+	}
+	return (msg);
+  }
 
+const server = http.createServer(function (req,res) {
 	let timestamp = new Date().toISOString();
 	console.log(`Incoming request ${req.method}, ${req.url} received at ${timestamp}`);
 
@@ -14,14 +22,13 @@ const server = http.createServer(function (req,res) {
 		case '/greetings/sayHello':
 		case '/greetings/sayHelloWithTime':
 			res.writeHead(200, {"Content-Type" : "text/html"});
-			res.write('<html><head><title>sayHello</title></head>');
-			res.write('<body><H1>' + greetingMsg + '</H1>');
-			
+
 			if (parsedURL.pathname == '/greetings/sayHelloWithTime') {
-				var today = new Date();
-				res.write('<p>It is now ' + today.toTimeString() + '</p>');
+				res.write(greetingMsg(parsedURL.query.name,true));
+			} else {
+				res.write(greetingMsg(parsedURL.query.name,false));
 			}
-			res.end('</body></html>');
+			res.end();
 			break;
 		default:
 			res.writeHead(404, {"Content-Type": "text/plain"});
