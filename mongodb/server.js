5e1850526337e6f4a5f6dd8b9f6610aa5e1ef5e8
Raymond SO <raymond.wc.so@gmail.com>
2021-05-13 07:51:32 +0800

    updated

--- mongodb/server.js
+++ mongodb/server.js
@@ -1,5 +1,6 @@
 const http = require('http');
-const url  = require('url');
+// const url  = require('url');
+const querystring = require('querystring');
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 const ObjectId = require('mongodb').ObjectID;
@@ -7,50 +8,54 @@ const mongoDBurl = '';
 
 const dbName = 'test';
 
-const server = http.createServer((req,res) => {
-	let timestamp = new Date().toISOString();
-	console.log(`Incoming request ${req.method}, ${req.url} received at ${timestamp}`);
+const server = http.createServer((req, res) => {
+	const timestamp = new Date().toISOString();
+	const { url, headers, method } = req;
 
-	let parsedURL = url.parse(req.url,true); // true to get query as object 
-	
-	switch(parsedURL.pathname) {
+	console.log(`Incoming request ${method}, ${url} received at ${timestamp}`);
+	const parsedURL = new URL(url, `http://${headers.host}`);
+
+	switch (parsedURL.pathname) {
 		case '/read':
 			read_n_print(res);
 			break;
 		case '/create':
 		case '/update':
 		case '/delete':
-			res.writeHead(500, {"Content-Type": "text/plain"});
+			res.writeHead(500, { "Content-Type": "text/plain" });
 			res.write(`${parsedURL.pathname} not available!!!`);
 			res.end();
 			break;
 		default:
-			res.writeHead(404, {"Content-Type": "text/plain"});
+			res.writeHead(404, { "Content-Type": "text/plain" });
 			res.write("404 Not Found\n");
 			res.end();
 	}
 });
 
-const findRestaurants = (db,callback) => {
-	cursor = db.collection('restaurants').find().limit(20); 
-	cursor.toArray((err,docs) => {
-		assert.equal(err,null);
+const findRestaurants = (db, callback) => {
+	cursor = db.collection('restaurants').find().limit(20);
+	cursor.toArray((err, docs) => {
+		assert.strictEqual(err, null);
 		//console.log(docs);
 		callback(docs);
 	});
 }
 
 const read_n_print = (res) => {
-	const client = new MongoClient(mongoDBurl);
+	const client = new MongoClient(mongoDBurl, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	});
 	client.connect((err) => {
-		assert.equal(null,err);
+		assert.strictEqual(null, err);
 		console.log("Connected successfully to server");
-		
+
 		const db = client.db(dbName);
 		findRestaurants(db, (restaurants) => {
 			client.close();
 			console.log('Disconnected MongoDB');
-			res.writeHead(200, {"Content-Type": "text/html"});
+			res.writeHead(200, { "Content-Type": "text/html" });
 			res.write('<html><head><title>Restaurant</title></head>');
 			res.write('<body><H1>Restaurants</H1>');
 			res.write(`<H2>Showing ${restaurants.length} document(s)</H2>`);
